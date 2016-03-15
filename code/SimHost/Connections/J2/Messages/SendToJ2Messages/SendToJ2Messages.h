f1ee23b4d51871f0d6ea83039ef1a32440dd9232
Trevor Hickey <thickey@f2si.net>
2016-03-15 11:44:10 -0400

    updates

---
+++ code/SimHost/Connections/J2/Messages/SendToJ2Messages/SendToJ2Messages.h
@@ -0,0 +1,61 @@
+/**
+*    @file
+*
+*    @author Trevor Hickey
+*
+*    @note Send To J2 Messages Interface
+*
+*    @brief Messages sent to J2
+*
+*    @details structures that make up all the messages that are sent to the J2 flight model
+*
+*    @copyright Copyright 2015.
+*    @n         Fidelity Flight Simulation, Inc. (F2Si)
+*    @n         All rights reserved.
+*
+*    @b Classification:      Unclassified
+*
+*    @b Project @b Name:     UH-72A Synthetic Flight Training System (SFTS)
+*
+*    @b Contract @b Number:  #W900KK-09-D-0577
+*
+*    @b Task @b Order:       0002
+*
+*    This work was generated under U.S. Government contract and the
+*    government has unlimited data rights therein.
+*
+*    @b DISTRIBUTION @b STATEMENT @b D:
+*    @n    Distribution authorized to the Department of Defense and U.S. DoD contractors only.
+*    @n    Other requests shall be referred to the controlling DoD office.
+*
+*    @b Organizations:
+*    @n    Fidelity Flight Simulation, Inc. (F2Si)
+*    @n    1815 Parkway View Drive
+*    @n    Pittsburgh, PA 15205
+*    @n    (412) 321-3280
+*
+*    @addtogroup J2 Communication
+*    @{
+*/
+
+#ifndef SendToJ2MessagesH
+#define SendToJ2MessagesH
+
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* #ifdef __cplusplus */
+
+
+
+
+#ifdef __cplusplus
+}
+#endif /* #ifdef __cplusplus */
+
+#endif /* SendToJ2MessagesH */
+
+///@}
+
